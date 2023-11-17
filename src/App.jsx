f61184fdaee5b04f4d9fd5eabc5d22c2a9f18aa1
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-17 17:19:35 +0700

    Đồng hộ Dark mode & Light Mode với System

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,68 @@
 import Button from "@mui/material/Button";
 import { AccessAlarm, ThreeDRotation } from "@mui/icons-material";
 import { useColorScheme } from "@mui/material/styles";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import Stack from "@mui/material/Stack";
+
+function ModeChange() {
+    const { mode, setMode } = useColorScheme();
+
+    const handleChange = (event) => {
+        const selectMode = event.target.value;
+        setMode(selectMode);
+    };
+
+    return (
+        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+            <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+            <Select
+                labelId="label-select-dark-light-mode"
+                id="label-select-dark-light-mode"
+                value={mode}
+                label="Mode"
+                onChange={handleChange}
+            >
+                <MenuItem value="light">
+                    <Stack
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={8}
+                    >
+                        <LightModeIcon fontSize="small" /> Light
+                    </Stack>
+                </MenuItem>
+                <MenuItem value="dark">
+                    <Stack
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={8}
+                    >
+                        <DarkModeIcon fontSize="small" />
+                        Dark
+                    </Stack>
+                </MenuItem>
+                <MenuItem value="System">
+                    <Stack
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                        spacing={8}
+                    >
+                        <SettingsBrightnessIcon fontSize="small" /> System
+                    </Stack>
+                </MenuItem>
+            </Select>
+        </FormControl>
+    );
+}
 
 function ModeToggle() {
     const { mode, setMode } = useColorScheme();
@@ -18,6 +80,7 @@ function ModeToggle() {
 function App() {
     return (
         <>
+            <ModeChange />
             <ModeToggle />
             <Button variant="contained">Hello world</Button>
             <AccessAlarm />
