864af47be2e6bd7490af324251914ecfbb8bd4fd
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-29 09:23:09 +0700

    Axios: Gọi API từ phía Front-end

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,16 +3,28 @@ import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "./BoardBar/BoardBar";
 import BoardContent from "./BoardContent/BoardContent";
 import { mockData } from "~/apis/mock-data";
+import { useEffect, useState } from "react";
+import { fetchBoardDetailsAPI } from "~/apis";
 const Board = () => {
-    return (
-        <>
-            <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
-                <AppBar />
-                <BoardBar board={mockData?.board} />
-                <BoardContent board={mockData?.board} />
-            </Container>
-        </>
-    );
+  const [board, setBoard] = useState(null);
+
+  useEffect(() => {
+    const boardId = "65660a7a9fc852451791631c";
+
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board);
+    });
+  }, []);
+
+  return (
+    <>
+      <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+        <AppBar />
+        <BoardBar board={board} />
+        <BoardContent board={board} />
+      </Container>
+    </>
+  );
 };
 
 export default Board;
